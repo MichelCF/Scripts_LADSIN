f0d4d69f73925c3b640977abb8f09699ac58a4c3
MichelCF <michel.caiafa@gmail.com>
2019-12-02 17:27:42 -0300

    Arrumando Caminhos

--- Automação Track/executa_track.py
+++ Automação Track/executa_track.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
-
-arquivos_dat = os.listdir('/home/ladsin/TRACK-1.5.0/indat')
+#Esse script deve estar na pasta principal do algoritimo TRACK
+print('Insira o caminho para pasta indat do TRACK')
+path_indat = input()
+print('Insira o caminho da pasta onde deseja salvar o resultado do TRACK')
+path_resultado = input()
+arquivos_dat = os.listdir(path_indat)
 arquivos_dat = [i for i in arquivos_dat if i.endswith('.dat')]
-print(arquivos_dat)
+
 for arquivo in arquivos_dat:
 
-    subprocess.call(' ./master -c='+arquivo.replace('.dat', '')+' -d=now -e=track.linux -i='+arquivo+ ' -f=vo1991 -n=1,746,1 -o=/home/ladsin/Documentos -r=RUN_AT_ -s=RUNDATIN.VOR -j=RUN_AT.in', shell=True)
+    subprocess.call(' ./master -c='+arquivo.replace('.dat', '')+' -d=now -e=track.linux -i='+arquivo+ 
+                    ' -f=vo1991 -n=1,746,1 -o='+path_resultado+' -r=RUN_AT_ -s=RUNDATIN.VOR -j=RUN_AT.in', shell=True)
     
--- Automação Track/gera_frames.py
+++ Automação Track/gera_frames.py
@@ -3,20 +3,28 @@ import shutil
 import subprocess
 
 #Caminhos do track
-TRACK_PRINCIPAL = '/home/ladsin/TRACK-1.5.0/'
-TRACK_INDAT = '/home/ladsin/TRACK-1.5.0/indat/'
-TRACK_OUTDAT = '/home/ladsin/TRACK-1.5.0/outdat/'
+print('Insira o caminho para a pasta principal do TRACK')
+TRACK_PRINCIPAL = input()
+print('Insira o caminho para a pasta indat do TRACK')
+TRACK_INDAT = input()
+print('Insira o caminho para a pasta outdat do TRACK')
+TRACK_OUTDAT = input()
 
 #Arquivo de saida de FRAME
-MAX_FRAME = '745'
+
 SAIDA_FRAME='specfil.filt_band000'
 
 #Dados de vorticidade
-DADOS_VORTICIDADE = '/work/archive/Dados - Vort Rel - 850 - Anne'
+print('Informe o caminho para a pasta com seus dados de vorticidade')
+DADOS_VORTICIDADE = input()
 
 #Pega apenas as pastas com os dados
 anos = os.listdir(DADOS_VORTICIDADE)
-anos = [ ano for ano in anos if len(ano)==4]
+anos = [ ano for ano in anos if len(ano)==4] #pensar em uma solução melhor
+
+#Pega o nome do arquivo specfiltT42
+print('informe o nome do seu arquivo de filtragem')
+specfiltT42 = input()
 
 
 for ano in anos:
@@ -25,9 +33,9 @@ for ano in anos:
         shutil.copy2(DADOS_VORTICIDADE + '/' + ano+ '/' + mes,
         TRACK_INDAT + mes)
         print (mes)
-        subprocess.call('bin/track.linux -i '+mes+' -f filt < Anne_specfiltT42.in', shell = True)
+        subprocess.call('bin/track.linux -i '+mes+' -f filt < '+specfiltT42, shell = True)
         os.remove(TRACK_INDAT+mes)
         shutil.move(TRACK_OUTDAT + SAIDA_FRAME, TRACK_INDAT)
-        final_frame = mes[0:19].replace('.nc', '.dat')
+        final_frame = mes[0:19].replace('.nc', '.dat')#pensar em uma solução melhor
         os.rename(TRACK_INDAT+SAIDA_FRAME,(TRACK_INDAT+final_frame).replace('.nc', '.dat'))
         
